25aab01394bb99da87a77e280319d4264f5b9dcc
eric <sky21c009@gsneotek.com>
2023-10-25 11:37:14 +0900

    8 commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,10 +1,19 @@
 import json
-import os
+import subprocess
+import wget
 
 def lambda_handler(event, context):
     # TODO implement
-    os.system('ls')
-    os.system('yum list installed | grep wgrib2')
+    command = 'ls -l'
+    # cmd = 'yum list installed | grep wgrib2'
+
+    # subprocess.run() 함수를 사용하여 명령어 실행
+    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # 실행 결과 출력
+    print("표준 출력:", result.stdout.decode())
+    print("표준 에러:", result.stderr.decode())
+    print("리턴 코드:", result.returncode)
 
     return {
         'statusCode': 200,
