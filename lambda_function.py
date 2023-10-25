cec676403bb12b1cfb76b5c494de1012b15ffb35
eric <sky21c009@gsneotek.com>
2023-10-25 10:09:38 +0900

    first commit

---
+++ lambda_function.py
@@ -0,0 +1,11 @@
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+	os.system('ls')
+	os.system('yum list installed | grep wgrib2')
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
